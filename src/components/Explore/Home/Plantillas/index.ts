d83a53ef218ddfff4ceb1ea9c8d2fcc278d60d5b
Ronny Toledo <ronnytoledo87@gmail.com>
2025-08-20 16:07:30 -0400

    $npm_config_message

--- src/components/Explore/Home/Plantillas/index.ts
+++
@@ -1,7 +0,0 @@
-export * as CardMinus from "./CardMinus";
-export * from "./CatalogosPost";
-export * from "./GridPorducts";
-export * from "./GridPost";
-export * from "./MostLike";
-export * from "./Popular";
-export * from "./PostCard";
